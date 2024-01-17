15aca4a2523134a0f813b31513365a848a7169c8
Tnkma <onwusilikegodswill@gmail.com>
2024-01-17 23:39:51 +0100

    tired and wants to sleep

--- models/base_model.py
+++ models/base_model.py
@@ -2,17 +2,19 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column,Integer, String
 
-
+Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
             from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.id = Column(str(uuid.uuid4(60)), nullable=False, primary_key=True)
+            self.created_at = Column(datetime.now(), nullable=False)
+            self.updated_at = Column(datetime.now(), nullable=False)
             storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
