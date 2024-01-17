1380d67e84e387c40840e15064a5628ee76a99bd
Tnkma <onwusilikegodswill@gmail.com>
2024-01-17 06:01:11 +0100

    fixing the database

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,12 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return list(FileStorage.__objects.values)
+        else:
+            return [obj for obj in FileStorage.__objects.values() if isinstance(obj, cls)]
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +51,10 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+        
+    def delete(self, obj=None):
+        """Delete obj is object inside or None"""
+        if obj is not None:
+            key = f'{obj.__class__.__name__}.{obj.id}'
+            if key in FileStorage.__objects:
+                del FileStorage.__objects[key]
